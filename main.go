4bf1bfb01b20fc7a7fc35287bfc92bdbf4163392
HystericalDragon <HystericalDragons@proton.me>
2024-01-13 09:24:36 +0800

    Use tag name
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- main.go
+++ main.go
@@ -389,7 +389,7 @@ func release(source string, destination string, output string, cnOutput string, 
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
+	setActionOutput("tag", *sourceRelease.TagName)
 	return nil
 }
 
